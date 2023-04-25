9b9d14e9b655d3b9826823c069f5846d14f5a5ee
Paulo Serrano <pvsserrano@gmail.com>
2023-04-25 19:05:47 -0300

    feat: toggle add task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/interfaces/Task';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-add-task',
@@ -10,8 +12,17 @@ export class AddTaskComponent {
   text: string = ""
   day: string = ""
   reminder: boolean = false
+  showAddTask!: boolean
+  subscription: Subscription
+
 
   @Output() onSubmit: EventEmitter<Omit<Task, "id">> = new EventEmitter()
+
+  constructor(private uiService: UiService) {
+		this.subscription = this.uiService.onToggle().subscribe(isShown => {
+			this.showAddTask = isShown
+		})
+  }
 
   public handleSubmit(){
     if (this.text.length === 0) {
